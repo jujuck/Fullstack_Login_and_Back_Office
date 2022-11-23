a00844dec2ad9443f6bac5909e5f0b9f724ee534
Julien ichard WCS <julien.y.richard@gmail.com>
2022-11-23 17:20:04 +0100

    Addint he user data on the back

--- backend/src/controllers/UsersController.js
+++ backend/src/controllers/UsersController.js
@@ -1,4 +1,12 @@
+const user = {
+  email: "trainer@wildcodeschool.com",
+  password: "WildCodeSchool2022",
+  name: "John Smith",
+  role: "admin",
+};
+
 const validateUser = (req, res) => {
+  console.error(user);
   res.send("OK");
 };
 
