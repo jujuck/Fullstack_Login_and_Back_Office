5c0edb13f06c78c9a05d3e470f65c6cfcfc8cb2f
Julien ichard WCS <julien.y.richard@gmail.com>
2022-11-23 17:26:21 +0100

    adding joi to check user fields

--- backend/src/controllers/UsersController.js
+++ backend/src/controllers/UsersController.js
@@ -1,3 +1,5 @@
+const Joi = require("joi");
+
 const user = {
   email: "trainer@wildcodeschool.com",
   password: "WildCodeSchool2022",
@@ -6,6 +8,14 @@ const user = {
 };
 
 const validateUser = (req, res) => {
+  const { error } = Joi.object({
+    email: Joi.string().email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net", "fr"] },
+    }),
+    password: Joi.string().min(8).max(25).required(),
+  }).validate(req.body, { abortEarly: false });
+  console.error(error);
   console.error(user);
   res.send("OK");
 };
