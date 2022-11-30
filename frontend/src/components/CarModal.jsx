d7f17c6f32c0e0428db8117ac1b3fa3e5a6d8eaa
KevinLavigne <lavigne.kevin02@gmail.com>
2022-11-30 16:23:25 +0100

    Create deleteCar function on CarModal.jsx .

--- frontend/src/components/CarModal.jsx
+++ frontend/src/components/CarModal.jsx
@@ -1,4 +1,16 @@
-function CarModal({ handleDisplay }) {
+import { useNavigate } from "react-router-dom";
+import apiConnexion from "@services/apiConnexion";
+
+function CarModal({ id, handleDisplay }) {
+  const navigate = useNavigate();
+
+  const deleteCar = () => {
+    apiConnexion
+      .delete(`/cars/${id}`)
+      .then(() => navigate("/"))
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div className=" w-full h-full z-[1] absolute top-0 flex justify-center items-center  ">
       <div className="w-4/5 h-4/5 bg-slate-400 p-4 rounded-[5rem] flex flex-col justify-center items-center">
@@ -6,8 +18,15 @@ function CarModal({ handleDisplay }) {
           Are you sure you want to delete this car ?
         </h1>
         <div className="flex justify-around w-full">
-          <button className="bg-red-700 py-2 px-4 rounded-3xl">yes</button>
           <button
+            type="button"
+            onClick={() => deleteCar()}
+            className="bg-red-700 py-2 px-4 rounded-3xl"
+          >
+            yes
+          </button>
+          <button
+            type="button"
             className="bg-green-500 py-2 px-4 rounded-3xl"
             onClick={() => handleDisplay()}
           >
